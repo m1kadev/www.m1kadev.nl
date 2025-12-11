03282dba0228b4270a1aba844de91df350e2e714
m1kadev <67912368+m1kadev@users.noreply.github.com>
2025-12-11 11:55:32 +0100

    ADD FXG INTEGRATIONNNN

--- build.exs
+++ build.exs
@@ -6,52 +6,61 @@ Mix.install([
 
 require EEx
 
+# hello
+
 Calendar.put_time_zone_database(Tzdata.TimeZoneDatabase)
 
 defmodule Builders do
   binfo_template = """
-  commit=<%= commit %>
-  lightningcss=<%= lightningcss %>
-  html_minifer_next=<%= html_minifier_next %>
-  uglifyjs=<%= uglifyjs %>
-  build_time=<%= build_time %>
+  commit=<%= commit %>lightningcss=<%= lightningcss %>html_minifer_next=<%= html_minifier_next %>uglifyjs=<%= uglifyjs %>build_time=<%= build_time %>
   """
 
   def html(input_path) do
     output_path = String.replace_prefix(input_path, "src", "build")
 
-    System.cmd("html-minifier-next", [
-      "--preset",
-      "conservative",
-      "-o",
-      output_path,
-      input_path
-    ])
+    {_, 0} =
+      System.cmd("html-minifier-next", [
+        "--preset",
+        "conservative",
+        "-o",
+        output_path,
+        input_path
+      ])
   end
 
   def css(input_path) do
     output_path = String.replace_prefix(input_path, "src", "build")
 
-    System.cmd("lightningcss", [
-      "-m",
-      "-o",
-      output_path,
-      input_path
-    ])
+    {_, 0} =
+      System.cmd("lightningcss", [
+        "-m",
+        "-o",
+        output_path,
+        input_path
+      ])
   end
 
   def js(input_path) do
     output_path = String.replace_prefix(input_path, "src", "build")
 
-    System.cmd("uglifyjs", [
-      "-c",
-      "-o",
-      output_path,
-      input_path
-    ])
+    {_, 0} =
+      System.cmd("uglifyjs", [
+        "-c",
+        "-o",
+        output_path,
+        input_path
+      ])
   end
 
-  # TODO: js support :)
+  def fxg(input_path, template) do
+    output_path =
+      String.replace_prefix(input_path, "src", "build")
+      |> String.replace_suffix("fxg", "html")
+
+    {output, 0} = System.cmd("fxg", [input_path])
+    output = String.replace(template, "<fxg-content />", output)
+    File.write(output_path, output)
+  end
 
   EEx.function_from_string(
     :def,
@@ -67,25 +76,13 @@ defmodule Builders do
   )
 end
 
-commit =
-  System.cmd("git", ["rev-parse", "HEAD"])
-  |> elem(0)
-  |> String.trim()
+{commit, 0} = System.cmd("lightningcss", ["-V"])
 
-lightningcss =
-  System.cmd("lightningcss", ["-V"])
-  |> elem(0)
-  |> String.trim()
+{lightningcss, 0} = System.cmd("lightningcss", ["-V"])
 
-html_minifier_next =
-  System.cmd("html-minifier-next", ["-V"])
-  |> elem(0)
-  |> String.trim()
+{html_minifier_next, 0} = System.cmd("html-minifier-next", ["-V"])
 
-uglifyjs =
-  System.cmd("uglifyjs", ["-V"])
-  |> elem(0)
-  |> String.trim()
+{uglifyjs, 0} = System.cmd("uglifyjs", ["-V"])
 
 File.rm_rf("build")
 
@@ -94,9 +91,11 @@ case File.mkdir_p("build/static") do
   _ -> raise "Couldn't create the build file structure (/build/static)"
 end
 
-Path.wildcard("src/**/*.html")
-|> Task.async_stream(fn file -> Builders.html(file) end)
-|> Enum.to_list()
+# Path.wildcard("src/**/*.html")
+# |> Task.async_stream(fn file -> Builders.html(file) end)
+# |> Enum.to_list()
+
+{:ok, main_template} = File.read("src/template.html")
 
 Path.wildcard("src/**/*.css")
 |> Task.async_stream(fn file -> Builders.css(file) end)
@@ -104,6 +103,10 @@ Path.wildcard("src/**/*.css")
 
 Path.wildcard("src/**/*.js")
 |> Task.async_stream(fn file -> Builders.js(file) end)
+|> Enum.to_list()
+
+Path.wildcard("src/**/*.fxg")
+|> Task.async_stream(fn file -> Builders.fxg(file, main_template) end)
 |> Enum.to_list()
 
 Path.wildcard("static/**/*")
